3b87b7605c94dd9923754acc9c34c0c5a0b26212
Caine Rotherham <rotherham.c@gmail.com>
2023-07-31 19:06:48 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export interface ListItem {
+  message?: string;
+  description?: string;
+  icon?: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'master-list-angular-app';
+
+  items: ListItem[] = [
+    { message: 'Randall Smokes', description: 'hi', icon: '/assets/icon.png' },
+    { message: 'Mikey House', description: 'hi', icon: '/assets/icon.png' },
+    { message: 'Spamzee', description: 'hi', icon: '/assets/icon.png' },
+    { message: 'Spamzee', description: 'hi', icon: '/assets/icon.png' },
+    { message: 'Spamzee', description: 'hi', icon: '/assets/icon.png' },
+  ];
 }
