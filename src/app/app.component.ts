381f30b90b0f1f0acbc5e3d07438d78962289664
Caine Rotherham <rotherham.c@gmail.com>
2023-08-02 18:38:08 +0200

    add feed and time intervals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { BehaviorSubject, interval } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 import { FeedService } from './feed.service';
 import settings from './settings.json';
+
+const ONE_MINUTE = 60000;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'master-list-angular-app';
   feeds = settings.feeds.map((feed) => ({
-    res$: this.feedService.getData(feed.endpoint),
+    res$: interval(ONE_MINUTE).pipe(
+      mergeMap(() => this.feedService.getData(feed.endpoint))
+    ),
   }));
+  date$ = new BehaviorSubject(new Date());
 
   constructor(protected feedService: FeedService) {}
+
+  ngAfterViewInit(): void {
+    setInterval(() => {
+      this.date$.next(new Date());
+    }, ONE_MINUTE);
+  }
 }
